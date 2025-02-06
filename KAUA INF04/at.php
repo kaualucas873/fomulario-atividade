de0915c5d6180e37541a8fc8c9e24c9ac562f34e
kaualucas873 <lkaua1331@gmail.com>
2025-02-06 16:01:27 -0300

    Add files via upload

---
+++ KAUA INF04/at.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="atv.css">
+</head >
+
+<body>
+    <div class= "quadrado">
+
+    <form action="action.php" method="post">
+    <label >Nome:</label>
+    <input name="name" id="nome" type="text">
+
+    <label>Idade:</label>
+    <input name="age" id="age" type="number">
+    
+    <label> Telefone </label>
+    <input name="age" id=telefone type="number">
+
+    <Label>Email</Label>
+    <input name=email type="text" id="email" >
+
+    <label>Endereço</label>
+    <input name=endereço type="text" id="endereço">
+    
+    <lavel>Mensagem</label>
+    <input name=mensagem type="text" id="mensagem"> 
+    
+    <button type="submit">Enviar</button>
+    </form> 
+</div>
+</html>
